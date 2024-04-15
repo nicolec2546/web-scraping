2f8b37da9eebf78c20a948aabdc541bacca562a5
nicolec2546 <144353597+nicolec2546@users.noreply.github.com>
2024-04-15 14:15:48 +0000

    yes

--- eastchester_scrape.py
+++ eastchester_scrape.py
@@ -5,3 +5,16 @@ import re
 
 eastchester_scrape = requests.get("https://www.eufsdk12.org/domain/76")
 soup = BeautifulSoup(eastchester_scrape.text,"html.parser")
+email_elements = soup.findAll("li",class_="staffemail")
+decoded_emails = []
+
+for element in email_elements: 
+    script = element.find('script').string if element.find('script') else None
+    if script:
+        matches = re.findall(r"swrot13\('([^']+)'\)",script)
+        if matches:
+            encoded_email = matches[0]
+            decoded_emails = codecs.decode[encoded_email,'rot_13']
+            decoded_emails.append(decoded_email)
+for email in decoded_emails:
+    print(email)
